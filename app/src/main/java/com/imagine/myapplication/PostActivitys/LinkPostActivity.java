81627103fa4862f1d8208f6c4171bc20e3a0652f
MatzS <matz.schultz@web.de>
2020-06-28 22:04:06 +0200

    LikeFixv1!

--- app/src/main/java/com/imagine/myapplication/PostActivitys/LinkPostActivity.java
+++ app/src/main/java/com/imagine/myapplication/PostActivitys/LinkPostActivity.java
@@ -3,6 +3,8 @@ package com.imagine.myapplication.PostActivitys;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,7 +22,9 @@ import com.imagine.myapplication.CommentsCallback;
 import com.imagine.myapplication.Feed.viewholder_classes.Helpers_Adapters.Post_Helper;
 import com.imagine.myapplication.Post_Fragment_Classes.LinkPostFragment;
 import com.imagine.myapplication.R;
+import com.imagine.myapplication.VoteHelper;
 import com.imagine.myapplication.post_classes.LinkPost;
+import com.imagine.myapplication.post_classes.Post;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -62,6 +66,7 @@ public class LinkPostActivity extends AppCompatActivity {
     }
 
     public void bind(){
+        init();
         TextView title_textView = findViewById(R.id.title_textView);
         TextView createTime_textView = findViewById(R.id.createDate_textView);
         TextView name_textView = findViewById(R.id.name_textView);
@@ -116,6 +121,72 @@ public class LinkPostActivity extends AppCompatActivity {
         Comments_Adapter adapter = new Comments_Adapter(comments,this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void init(){
+        final VoteHelper vote = new VoteHelper();
+
+        ImageButton thanksButton = findViewById(R.id.thanks_button);
+        ImageButton wowButton = findViewById(R.id.wow_button);
+        ImageButton haButton = findViewById(R.id.ha_button);
+        ImageButton niceButton = findViewById(R.id.nice_button);
+
+        thanksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vote.updateVotes("thanks",post);
+                upDateButtonUI("thanks",post);
+            }
+        });
+        wowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vote.updateVotes("wow",post);
+                upDateButtonUI("wow",post);
+            }
+        });
+        haButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vote.updateVotes("ha",post);
+                upDateButtonUI("ha",post);
+            }
+        });
+        niceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vote.updateVotes("nice",post);
+                upDateButtonUI("nice",post);
+            }
+        });
+    }
+
+    public void upDateButtonUI(String type, Post post){
+        switch(type){
+            case "thanks":
+                ImageButton thanksBUtton = findViewById(R.id.thanks_button);
+                thanksBUtton.setBackground(null);
+//                thanksBUtton.setText(post.thanksCount+"");
+                break;
+            case "wow":
+                ImageButton wowButton = findViewById(R.id.wow_button);
+                wowButton.setBackground(null);
+//                wowButton.setText(post.wowCount+"");
+                break;
+            case "ha":
+                ImageButton haButton = findViewById(R.id.ha_button);
+                haButton.setBackground(null);
+//                haButton.setText(post.haCount+"");
+                break;
+            case "nice":
+                ImageButton niceButton = findViewById(R.id.nice_button);
+                niceButton.setBackground(null);
+//                niceButton.setText(post.niceCount+"");
+                break;
+            default:
+                System.out.println("DEFAULT!");
+                break;
+        }
     }
 
     public String dateToString(Timestamp timestamp){
